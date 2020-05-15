943b88ab0a7bd502bc42d216d9c5c07d34dc7758
Dani Coleman <danicoleman00@gmail.com>
2020-05-14 19:14:25 -0600

    Add last two test before class victim

--- mythical-creatures/test/werewolf_test.rb
+++ mythical-creatures/test/werewolf_test.rb
@@ -10,73 +10,71 @@ class WerewolfTest < Minitest::Test
   end
 
   def test_it_has_a_location
-    skip
     werewolf = Werewolf.new("David","London")
     assert_equal "London", werewolf.location
   end
 
   def test_it_is_by_default_in_human_form
-    skip
     werewolf = Werewolf.new("David","London")
-    assert werewolf.human?
+    assert_equal true, werewolf.human?
   end
 
   def test_when_starting_as_a_human_changing_means_it_is_no_longer_human
-    skip
     werewolf = Werewolf.new("David","London")
     werewolf.change!
-    refute werewolf.human?
+    assert_equal false, werewolf.human?
   end
 
   def test_when_starting_as_a_human_changing_turns_it_into_a_werewolf
-    skip
     werewolf = Werewolf.new("David","London")
     werewolf.change!
-    assert werewolf.wolf?
+    assert_equal true, werewolf.wolf?
   end
 
   def test_when_starting_as_a_human_changing_a_second_time_it_becomes_human_again
-    skip
     werewolf = Werewolf.new("David","London")
     assert werewolf.human?
     werewolf.change!
     werewolf.change!
-    assert werewolf.human?
+    assert_equal true, werewolf.human?
   end
 
   def test_when_starting_as_a_werewolf_changing_a_second_time_it_becomes_werewolf_again
-    skip
     werewolf = Werewolf.new("David","London")
     werewolf.change!
     assert werewolf.wolf?
     werewolf.change!
     werewolf.change!
-    assert werewolf.wolf?
+    assert_equal true, werewolf.wolf?
   end
 
   def test_is_not_hungry_by_default
-    skip
-    # your code here
+    werewolf = Werewolf.new("David","London")
+
+    assert_equal false, werewolf.hungry?
   end
 
   def test_becomes_hungry_after_changing_to_a_werewolf
-    skip
-    # your code here
+    werewolf = Werewolf.new("David","London")
+    werewolf.change!
+
+    assert_equal true, werewolf.hungry?
   end
 
-  class Victim
-    attr_accessor :status
+  def test_it_is_a_victim
+    werewolf = Werewolf.new("David","London")
+    victim = Victim.new
 
-    def initialize
-      @status = :alive
-    end
+    assert_instance_of Victim, victim
   end
 
   def test_consumes_a_victim
-    skip
-    # your code here
+    werewolf = Werewolf.new("David","London")
+    victim = Victim.new
+
+    
   end
-  
+
   def test_cannot_consume_victim_if_in_human_form
     skip
     # your code here
